fc9c38a74a16a1acea98d1b5b48e031c34e2799b
ZokirjonovaMuslima <muslimazokirjonova2004@gmail.com>
2023-07-13 22:52:18 +0500

    create TODO

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -4,7 +4,6 @@ from rest_framework.routers import DefaultRouter
 from apps.users.views import (RegisterAPIView, LogoutAPIView)
 
 routers = DefaultRouter()
-# routers.register('orders/', OrderModelViewSet, 'orders')
 urlpatterns = [
     path('', include(routers.urls)),
     path('register/', RegisterAPIView.as_view(), name='register'),
