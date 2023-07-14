c0154501e78014bb61af4bf6a74e4158543de0d7
ZokirjonovaMuslima <muslimazokirjonova2004@gmail.com>
2023-07-14 11:16:56 +0500

    created

--- apps/users/views.py
+++ apps/users/views.py
@@ -10,46 +10,6 @@ from rest_framework_simplejwt.tokens import RefreshToken
 from apps.users.models import Teacher, Rating, Subscribe, Wishlist
 from apps.users.serializers import (TeacherModelSerializer, TeacherDetailModelSerializer, RatingModelSerializer,
                                     SubscriptionSerializer, WishListModelSerializer)
-from apps.users.services import register_service, reset_password_service, reset_password_confirm_service
-
-
-# User/Register API
-class RegisterAPIView(APIView):
-    def post(self, request):
-        response = register_service(request.data)
-        if response['success']:
-            return Response(status=201)
-        return Response(response, status=405)
-
-
-#  User/Reset Password API
-class ResetPasswordAPIView(APIView):
-    def post(self, request):
-        responce = reset_password_service(request)
-        if responce['success']:
-            return Response({'message': 'sent'})
-        return Response(responce, status=404)
-
-
-# User/Reset Password Confirm API
-
-class PasswordResetConfirmAPIView(APIView):
-
-    def post(self, request, token, uuid):
-        response = reset_password_confirm_service(request, token, uuid)
-        if response['success']:
-            return Response({'message': 'Password changed'})
-        return Response(response, status=400)
-
-
-# User/Logout API
-class LogoutAPIView(APIView):
-    permission_classes = (IsAuthenticated,)
-
-    def post(self, request):
-        token = RefreshToken(request.user)
-        token.blacklist()
-        return Response(status=200)
 
 
 #  Teacher/List API
